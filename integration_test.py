f8a497e333f091e687189649d31d3ad18e5d466c
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2023-11-19 12:24:44 +0300

    Initial commit

---
+++ integration_test.py
@@ -0,0 +1,179 @@
+"""Интеграционные тесты транслятора и машины.
+
+В данном модуле использованы два подхода для тестирования:
+
+- golden tests (`test_translator_and_machine`) -- легковесные и удобные (рекомендуется
+  для выполнения задачи)
+
+- традиционные тесты (`TestTranslatorAndMachine`) -- тяжеловесные и не удобные (не требуются
+  для выполнения задачи)
+
+- дополнительно рекомендуется делать unit-тесты для отдельных функций, где это
+целесообразно
+"""
+
+import contextlib
+import io
+import logging
+import os
+import tempfile
+import unittest
+
+import machine
+import pytest
+import translator
+
+
+@pytest.mark.golden_test("golden/*.yml")
+def test_translator_and_machine(golden, caplog):
+    """Используется подход golden tests. У него не самая удачная реализация для
+    python: https://pypi.org/project/pytest-golden/ , но знать об этом подходе
+    крайне полезно.
+
+    Принцип работы следующий: во внешних файлах специфицируются входные и
+    выходные данные для теста. При запуске тестов происходит сравнение и если
+    выход изменился -- выводится ошибка.
+
+    Если вы меняете логику работы приложения -- то запускаете тесты с ключом:
+    `cd src/brainfuck && poetry run pytest . -v --update-goldens`
+
+    Это обновит файлы конфигурации, и вы можете закоммитить изменения в
+    репозиторий, если они корректные.
+
+    Формат файла описания теста -- YAML. Поля определяются доступом из теста к
+    аргументу `golden` (`golden[key]` -- входные данные, `golden.out("key")` --
+    выходные данные).
+
+    Вход:
+
+    - `in_source` -- исходный код
+    - `in_stdin` -- данные на ввод процессора для симуляции
+
+    Выход:
+
+    - `out_code` -- машинный код, сгенерированный транслятором
+    - `out_stdout` -- стандартный вывод транслятора и симулятора
+    - `out_log` -- журнал программы
+    """
+    # Установим уровень отладочного вывода на DEBUG
+    caplog.set_level(logging.DEBUG)
+
+    # Создаём временную папку для тестирования приложения.
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        # Готовим имена файлов для входных и выходных данных.
+        source = os.path.join(tmpdirname, "source.bf")
+        input_stream = os.path.join(tmpdirname, "input.txt")
+        target = os.path.join(tmpdirname, "target.o")
+
+        # Записываем входные данные в файлы. Данные берутся из теста.
+        with open(source, "w", encoding="utf-8") as file:
+            file.write(golden["in_source"])
+        with open(input_stream, "w", encoding="utf-8") as file:
+            file.write(golden["in_stdin"])
+
+        # Запускаем транслятор и собираем весь стандартный вывод в переменную
+        # stdout
+        with contextlib.redirect_stdout(io.StringIO()) as stdout:
+            translator.main(source, target)
+            print("============================================================")
+            machine.main(target, input_stream)
+
+        # Выходные данные также считываем в переменные.
+        with open(target, encoding="utf-8") as file:
+            code = file.read()
+
+        # Проверяем, что ожидания соответствуют реальности.
+        assert code == golden.out["out_code"]
+        assert stdout.getvalue() == golden.out["out_stdout"]
+        assert caplog.text == golden.out["out_log"]
+
+
+class TestTranslatorAndMachine(unittest.TestCase):
+    """Данные тесты оставлены для общего развития. Для выполнения лабораторной
+    работы они не требуются.
+
+    В задании лабораторной работы необходимо использовать golden tests.
+    """
+
+    def test_hello_example(self):
+        # Создаём временную папку для скомпилированного файла. Удаляется автоматически.
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            source = "examples/hello.bf"
+            target = os.path.join(tmpdirname, "machine_code.out")
+            input_stream = "examples/input.txt"
+
+            # Собираем весь стандартный вывод в переменную stdout.
+            with contextlib.redirect_stdout(io.StringIO()) as stdout:
+                translator.main(source, target)
+                machine.main(target, input_stream)
+
+            # Проверяем, что было напечатано то, что мы ожидали.
+            assert (
+                stdout.getvalue()
+                == "source LoC: 357 code instr: 131\nHello World!\n\ninstr_counter:  987 ticks: 1532\n"
+            )
+
+    def test_cat_example(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            source = "examples/cat.bf"
+            target = os.path.join(tmpdirname, "machine_code.out")
+            input_stream = "examples/input.txt"
+
+            with contextlib.redirect_stdout(io.StringIO()) as stdout:
+                # Собираем журнал событий по уровню INFO в переменную logs.
+                with self.assertLogs("", level="INFO") as logs:
+                    translator.main(source, target)
+                    machine.main(target, input_stream)
+
+            assert (
+                stdout.getvalue()
+                == "source LoC: 1 code instr: 6\nHello World from input!\n\ninstr_counter:  95 ticks: 168\n"
+            )
+
+            assert logs.output == [
+                "WARNING:root:Input buffer is empty!",
+                "INFO:root:output_buffer: 'Hello World from input!\\n'",
+            ]
+
+    def test_cat_example_log(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            source = "examples/cat.bf"
+            target = os.path.join(tmpdirname, "machine_code.out")
+            input_stream = "examples/foo_input.txt"
+
+            with contextlib.redirect_stdout(io.StringIO()) as stdout:
+                with self.assertLogs("", level="DEBUG") as logs:
+                    translator.main(source, target)
+                    machine.main(target, input_stream)
+
+            assert stdout.getvalue() == "source LoC: 1 code instr: 6\nfoo\n\ninstr_counter:  15 ticks: 28\n"
+
+            expect_log = [
+                "DEBUG:root:TICK:   0 PC:   0 ADDR:   0 MEM_OUT: 0 ACC: 0 \tinput  (','@1:1)",
+                "DEBUG:root:input: 'f'",
+                "DEBUG:root:TICK:   2 PC:   1 ADDR:   0 MEM_OUT: 102 ACC: 0 \tjz 5  ('['@1:2)",
+                "DEBUG:root:TICK:   4 PC:   2 ADDR:   0 MEM_OUT: 102 ACC: 102 \tprint  ('.'@1:3)",
+                "DEBUG:root:output: '' << 'f'",
+                "DEBUG:root:TICK:   6 PC:   3 ADDR:   0 MEM_OUT: 102 ACC: 102 \tinput  (','@1:4)",
+                "DEBUG:root:input: 'o'",
+                "DEBUG:root:TICK:   8 PC:   4 ADDR:   0 MEM_OUT: 111 ACC: 102 \tjmp 1  (']'@1:5)",
+                "DEBUG:root:TICK:   9 PC:   1 ADDR:   0 MEM_OUT: 111 ACC: 102 \tjz 5  ('['@1:2)",
+                "DEBUG:root:TICK:  11 PC:   2 ADDR:   0 MEM_OUT: 111 ACC: 111 \tprint  ('.'@1:3)",
+                "DEBUG:root:output: 'f' << 'o'",
+                "DEBUG:root:TICK:  13 PC:   3 ADDR:   0 MEM_OUT: 111 ACC: 111 \tinput  (','@1:4)",
+                "DEBUG:root:input: 'o'",
+                "DEBUG:root:TICK:  15 PC:   4 ADDR:   0 MEM_OUT: 111 ACC: 111 \tjmp 1  (']'@1:5)",
+                "DEBUG:root:TICK:  16 PC:   1 ADDR:   0 MEM_OUT: 111 ACC: 111 \tjz 5  ('['@1:2)",
+                "DEBUG:root:TICK:  18 PC:   2 ADDR:   0 MEM_OUT: 111 ACC: 111 \tprint  ('.'@1:3)",
+                "DEBUG:root:output: 'fo' << 'o'",
+                "DEBUG:root:TICK:  20 PC:   3 ADDR:   0 MEM_OUT: 111 ACC: 111 \tinput  (','@1:4)",
+                "DEBUG:root:input: '\\n'",
+                "DEBUG:root:TICK:  22 PC:   4 ADDR:   0 MEM_OUT: 10 ACC: 111 \tjmp 1  (']'@1:5)",
+                "DEBUG:root:TICK:  23 PC:   1 ADDR:   0 MEM_OUT: 10 ACC: 111 \tjz 5  ('['@1:2)",
+                "DEBUG:root:TICK:  25 PC:   2 ADDR:   0 MEM_OUT: 10 ACC: 10 \tprint  ('.'@1:3)",
+                "DEBUG:root:output: 'foo' << '\\n'",
+                "DEBUG:root:TICK:  27 PC:   3 ADDR:   0 MEM_OUT: 10 ACC: 10 \tinput  (','@1:4)",
+                "WARNING:root:Input buffer is empty!",
+                "INFO:root:output_buffer: 'foo\\n'",
+            ]
+            assert logs.output == expect_log
---
+++ machine.py
@@ -0,0 +1,429 @@
+#!/usr/bin/python3
+"""Модель процессора, позволяющая выполнить машинный код полученный из программы
+на языке Brainfuck.
+
+Модель включает в себя три основных компонента:
+
+- `DataPath` -- работа с памятью данных и вводом-выводом.
+
+- `ControlUnit` -- работа с памятью команд и их интерпретация.
+
+- и набор вспомогательных функций: `simulation`, `main`.
+"""
+
+import logging
+import sys
+
+from isa import Opcode, read_code
+
+
+class DataPath:
+    """Тракт данных (пассивный), включая: ввод/вывод, память и арифметику.
+
+    ```text
+     latch --------->+--------------+  addr   +--------+
+     data            | data_address |---+---->|  data  |
+     addr      +---->+--------------+   |     | memory |
+               |                        |     |        |
+           +-------+                    |     |        |
+    sel -->|  MUX  |         +----------+     |        |
+           +-------+         |                |        |
+            ^     ^          |                |        |
+            |     |          |        data_in |        | data_out
+            |     +---(+1)---+          +---->|        |-----+
+            |                |          |     |        |     |
+            +---------(-1)---+          |  oe |        |     |
+                                        | --->|        |     |
+                                        |     |        |     |
+                                        |  wr |        |     |
+                                        | --->|        |     |
+                                        |     +--------+     |
+                                        |                    v
+                                    +--------+  latch_acc +-----+
+                          sel ----> |  MUX   |  --------->| acc |
+                                    +--------+            +-----+
+                                     ^   ^  ^                |
+                                     |   |  |                +---(==0)---> zero
+                                     |   |  |                |
+                                     |   |  +---(+1)---------+
+                                     |   |                   |
+                                     |   +------(-1)---------+
+                                     |                       |
+            input -------------------+                       +---------> output
+    ```
+
+    - data_memory -- однопортовая, поэтому либо читаем, либо пишем.
+
+    - input/output -- токенизированная логика ввода-вывода. Не детализируется в
+      рамках модели.
+
+    - input -- чтение может вызвать остановку процесса моделирования, если буфер
+      входных значений закончился.
+
+    Реализованные методы соответствуют сигналам защёлкивания значений:
+
+    - `signal_latch_data_addr` -- защёлкивание адреса в памяти данных;
+    - `signal_latch_acc` -- защёлкивание аккумулятора;
+    - `signal_wr` -- запись в память данных;
+    - `signal_output` -- вывод в порт.
+
+    Сигнал "исполняется" за один такт. Корректность использования сигналов --
+    задача `ControlUnit`.
+    """
+
+    data_memory_size = None
+    "Размер памяти данных."
+
+    data_memory = None
+    "Память данных. Инициализируется нулевыми значениями."
+
+    data_address = None
+    "Адрес в памяти данных. Инициализируется нулём."
+
+    acc = None
+    "Аккумулятор. Инициализируется нулём."
+
+    input_buffer = None
+    "Буфер входных данных. Инициализируется входными данными конструктора."
+
+    output_buffer = None
+    "Буфер выходных данных."
+
+    def __init__(self, data_memory_size, input_buffer):
+        assert data_memory_size > 0, "Data_memory size should be non-zero"
+        self.data_memory_size = data_memory_size
+        self.data_memory = [0] * data_memory_size
+        self.data_address = 0
+        self.acc = 0
+        self.input_buffer = input_buffer
+        self.output_buffer = []
+
+    def signal_latch_data_addr(self, sel):
+        """Защёлкнуть адрес в памяти данных. Защёлкивание осуществляется на
+        основе селектора `sel` в котором указывается `Opcode`:
+
+        - `Opcode.LEFT.value` -- сдвиг влево;
+
+        - `Opcode.RIGHT.value` -- сдвиг вправо.
+
+        При выходе за границы памяти данных процесс моделирования останавливается.
+        """
+        assert sel in {Opcode.LEFT.value, Opcode.RIGHT.value}, "internal error, incorrect selector: {}".format(sel)
+
+        if sel == Opcode.LEFT.value:
+            self.data_address -= 1
+        elif sel == Opcode.RIGHT.value:
+            self.data_address += 1
+
+        assert 0 <= self.data_address < self.data_memory_size, "out of memory: {}".format(self.data_address)
+
+    def signal_latch_acc(self):
+        """Защёлкнуть слово из памяти (`oe` от Output Enable) и защёлкнуть его в
+        аккумулятор. Сигнал `oe` выставляется неявно `ControlUnit`-ом.
+        """
+        self.acc = self.data_memory[self.data_address]
+
+    def signal_wr(self, sel):
+        """wr (от WRite), сохранить в память.
+
+        Запись в память осуществляется на основе селектора `sel` в котором указывается `Opcode`:
+
+        - `Opcode.INC.value` -- инкремент аккумулятора;
+
+        - `Opcode.DEC.value` -- декремент аккумулятора;
+
+        - `Opcode.INPUT.value` -- ввод из буфера входных данных. При исчерпании
+          буфера -- выбрасывается исключение `EOFError`.
+
+        В примере ниже имитируется переполнение ячейки при инкременте. Данный
+        текст является doctest-ом, корректность которого проверяется во время
+        загрузки модуля или командой: `python3 -m doctest -v machine.py`
+
+        >>> dp = DataPath(10, [chr(127)])
+        >>> dp.signal_wr(Opcode.INPUT.value)
+        >>> dp.signal_latch_acc()
+        >>> dp.acc
+        127
+        >>> dp.signal_wr(Opcode.INC.value)
+        >>> dp.signal_latch_acc()
+        >>> dp.acc
+        -128
+
+        """
+        assert sel in {
+            Opcode.INC.value,
+            Opcode.DEC.value,
+            Opcode.INPUT.value,
+        }, "internal error, incorrect selector: {}".format(sel)
+
+        if sel == Opcode.INC.value:
+            self.data_memory[self.data_address] = self.acc + 1
+            if self.data_memory[self.data_address] == 128:
+                self.data_memory[self.data_address] = -128
+        elif sel == Opcode.DEC.value:
+            self.data_memory[self.data_address] = self.acc - 1
+            if self.data_memory[self.data_address] == -129:
+                self.data_memory[self.data_address] = 127
+        elif sel == Opcode.INPUT.value:
+            if len(self.input_buffer) == 0:
+                raise EOFError()
+            symbol = self.input_buffer.pop(0)
+            symbol_code = ord(symbol)
+            assert -128 <= symbol_code <= 127, "input token is out of bound: {}".format(symbol_code)
+            self.data_memory[self.data_address] = symbol_code
+            logging.debug("input: %s", repr(symbol))
+
+    def signal_output(self):
+        """Вывести значение аккумулятора в порт вывода.
+
+        Вывод осуществляется путём конвертации значения аккумулятора в символ по
+        ASCII-таблице.
+        """
+        symbol = chr(self.acc)
+        logging.debug("output: %s << %s", repr("".join(self.output_buffer)), repr(symbol))
+        self.output_buffer.append(symbol)
+
+    def zero(self):
+        """Флаг нуля. Необходим для условных переходов."""
+        return self.acc == 0
+
+
+class ControlUnit:
+    """Блок управления процессора. Выполняет декодирование инструкций и
+    управляет состоянием модели процессора, включая обработку данных (DataPath).
+
+    Согласно варианту, любая инструкция может быть закодирована в одно слово.
+    Следовательно, индекс памяти команд эквивалентен номеру инструкции.
+
+    ```text
+    +------------------(+1)-------+
+    |                             |
+    |   +-----+                   |
+    +-->|     |     +---------+   |    +---------+
+        | MUX |---->| program |---+--->| program |
+    +-->|     |     | counter |        | memory  |
+    |   +-----+     +---------+        +---------+
+    |      ^                               |
+    |      | sel_next                      | current instruction
+    |      |                               |
+    +---------------(select-arg)-----------+
+           |                               |      +---------+
+           |                               |      |  step   |
+           |                               |  +---| counter |
+           |                               |  |   +---------+
+           |                               v  v        ^
+           |                       +-------------+     |
+           +-----------------------| instruction |-----+
+                                   |   decoder   |
+                                   |             |<-------+
+                                   +-------------+        |
+                                           |              |
+                                           | signals      |
+                                           v              |
+                                     +----------+  zero   |
+                                     |          |---------+
+                                     | DataPath |
+                      input -------->|          |----------> output
+                                     +----------+
+    ```
+
+    """
+
+    program = None
+    "Память команд."
+
+    program_counter = None
+    "Счётчик команд. Инициализируется нулём."
+
+    data_path = None
+    "Блок обработки данных."
+
+    _tick = None
+    "Текущее модельное время процессора (в тактах). Инициализируется нулём."
+
+    def __init__(self, program, data_path):
+        self.program = program
+        self.program_counter = 0
+        self.data_path = data_path
+        self._tick = 0
+
+    def tick(self):
+        """Продвинуть модельное время процессора вперёд на один такт."""
+        self._tick += 1
+
+    def current_tick(self):
+        """Текущее модельное время процессора (в тактах)."""
+        return self._tick
+
+    def signal_latch_program_counter(self, sel_next):
+        """Защёлкнуть новое счётчика команд.
+
+        Если `sel_next` равен `True`, то счётчик будет увеличен на единицу,
+        иначе -- будет установлен в значение аргумента текущей инструкции.
+        """
+        if sel_next:
+            self.program_counter += 1
+        else:
+            instr = self.program[self.program_counter]
+            assert "arg" in instr, "internal error"
+            self.program_counter = instr["arg"]
+
+    def decode_and_execute_control_flow_instruction(self, instr, opcode):
+        """Декодировать и выполнить инструкцию управления потоком исполнения. В
+        случае успеха -- вернуть `True`, чтобы перейти к следующей инструкции.
+        """
+        if opcode is Opcode.HALT:
+            raise StopIteration()
+
+        if opcode is Opcode.JMP:
+            addr = instr["arg"]
+            self.program_counter = addr
+            self.tick()
+
+            return True
+
+        if opcode is Opcode.JZ:
+            addr = instr["arg"]
+
+            self.data_path.signal_latch_acc()
+            self.tick()
+
+            if self.data_path.zero():
+                self.signal_latch_program_counter(sel_next=False)
+            else:
+                self.signal_latch_program_counter(sel_next=True)
+            self.tick()
+
+            return True
+
+        return False
+
+    def decode_and_execute_instruction(self):
+        """Основной цикл процессора. Декодирует и выполняет инструкцию.
+
+        Обработка инструкции:
+
+        1. Проверить `Opcode`.
+
+        2. Вызвать методы, имитирующие необходимые управляющие сигналы.
+
+        3. Продвинуть модельное время вперёд на один такт (`tick`).
+
+        4. (если необходимо) повторить шаги 2-3.
+
+        5. Перейти к следующей инструкции.
+
+        Обработка функций управления потоком исполнения вынесена в
+        `decode_and_execute_control_flow_instruction`.
+        """
+        instr = self.program[self.program_counter]
+        opcode = instr["opcode"]
+
+        if self.decode_and_execute_control_flow_instruction(instr, opcode):
+            return
+
+        if opcode in {Opcode.RIGHT, Opcode.LEFT}:
+            self.data_path.signal_latch_data_addr(opcode.value)
+            self.signal_latch_program_counter(sel_next=True)
+            self.tick()
+
+        elif opcode in {Opcode.INC, Opcode.DEC, Opcode.INPUT}:
+            self.data_path.signal_latch_acc()
+            self.tick()
+
+            self.data_path.signal_wr(opcode.value)
+            self.signal_latch_program_counter(sel_next=True)
+            self.tick()
+
+        elif opcode is Opcode.PRINT:
+            self.data_path.signal_latch_acc()
+            self.tick()
+
+            self.data_path.signal_output()
+            self.signal_latch_program_counter(sel_next=True)
+            self.tick()
+
+    def __repr__(self):
+        """Вернуть строковое представление состояния процессора."""
+        state_repr = "TICK: {:3} PC: {:3} ADDR: {:3} MEM_OUT: {} ACC: {}".format(
+            self._tick,
+            self.program_counter,
+            self.data_path.data_address,
+            self.data_path.data_memory[self.data_path.data_address],
+            self.data_path.acc,
+        )
+
+        instr = self.program[self.program_counter]
+        opcode = instr["opcode"]
+        instr_repr = str(opcode)
+
+        if "arg" in instr:
+            instr_repr += " {}".format(instr["arg"])
+
+        if "term" in instr:
+            term = instr["term"]
+            instr_repr += "  ('{}'@{}:{})".format(term.symbol, term.line, term.pos)
+
+        return "{} \t{}".format(state_repr, instr_repr)
+
+
+def simulation(code, input_tokens, data_memory_size, limit):
+    """Подготовка модели и запуск симуляции процессора.
+
+    Длительность моделирования ограничена:
+
+    - количеством выполненных инструкций (`limit`);
+
+    - количеством данных ввода (`input_tokens`, если ввод используется), через
+      исключение `EOFError`;
+
+    - инструкцией `Halt`, через исключение `StopIteration`.
+    """
+    data_path = DataPath(data_memory_size, input_tokens)
+    control_unit = ControlUnit(code, data_path)
+    instr_counter = 0
+
+    logging.debug("%s", control_unit)
+    try:
+        while instr_counter < limit:
+            control_unit.decode_and_execute_instruction()
+            instr_counter += 1
+            logging.debug("%s", control_unit)
+    except EOFError:
+        logging.warning("Input buffer is empty!")
+    except StopIteration:
+        pass
+
+    if instr_counter >= limit:
+        logging.warning("Limit exceeded!")
+    logging.info("output_buffer: %s", repr("".join(data_path.output_buffer)))
+    return "".join(data_path.output_buffer), instr_counter, control_unit.current_tick()
+
+
+def main(code_file, input_file):
+    """Функция запуска модели процессора. Параметры -- имена файлов с машинным
+    кодом и с входными данными для симуляции.
+    """
+    code = read_code(code_file)
+    with open(input_file, encoding="utf-8") as file:
+        input_text = file.read()
+        input_token = []
+        for char in input_text:
+            input_token.append(char)
+
+    output, instr_counter, ticks = simulation(
+        code,
+        input_tokens=input_token,
+        data_memory_size=100,
+        limit=1000,
+    )
+
+    print("".join(output))
+    print("instr_counter: ", instr_counter, "ticks:", ticks)
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.DEBUG)
+    assert len(sys.argv) == 3, "Wrong arguments: machine.py <code_file> <input_file>"
+    _, code_file, input_file = sys.argv
+    main(code_file, input_file)
